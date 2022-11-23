2ddd3bbc9d46576975af6d782816e9427c977e4b
Tom <artzhubrikov@mail.ru>
2022-11-23 18:24:15 +0300

    Homework completed

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Введите два числа...\nВведите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
+
+if (n > m) Console.Write($"max = {n}");
+else if (n == m) Console.Write("Два числа равны!");
+else Console.Write($"max = {m}");
---
+++ Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите три числа...\nВведите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numC = int.Parse(Console.ReadLine());
+
+int max = numA;
+
+if (max < numB) max = numB;
+if (max < numC) max = numC;
+Console.Write($"max = {max}");
